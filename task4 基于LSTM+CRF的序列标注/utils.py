0a6ab3b93adaaecfe45f697f36248bfab974e4f0
Raki <64342701+Raki-j@users.noreply.github.com>
2022-01-20 18:54:34 +0800

    Update utils.py

--- task4 基于LSTM+CRF的序列标注/utils.py
+++ task4 基于LSTM+CRF的序列标注/utils.py
@@ -46,8 +46,7 @@ class mydataset(Dataset):
         data = self.x[index]
         labels = self.y[index]
         length = self.length_list[index]
-        return torch.tensor(data), torch.tensor(labels), torch.tensor(length)
-        #return data, labels, length
+        return data, labels, length
     def __len__(self):
         return len(self.x)
 
@@ -67,40 +66,64 @@ def padding(x, max_length, d):
     return x
 
 def get_dataloader(x, y, batch_size):
+    word2idx, tag2idx, vocab_size = pre_processing()
     inputs = [sentence2vector(s, word2idx) for s in x] # 每一个句子都转化成vector
-
     targets = [sentence2vector(s, tag2idx) for s in y]
 
     length_list = [len(sentence) for sentence in inputs]
 
     max_length = 0
-
     max_length = max(max(length_list), max_length)
+    max_length = 124
 
     inputs = torch.tensor([padding(sentence, max_length, word2idx) for sentence in inputs])
-
     targets = torch.tensor([padding(sentence, max_length, tag2idx) for sentence in targets], dtype=torch.long)
 
     dataset = mydataset(inputs, targets, length_list)
-
     dataloader = DataLoader(dataset, shuffle=False, batch_size=batch_size)
 
     return dataloader, max_length
 
 def pre_processing():
-    x_train, y_train = read_data("data/conll2003/train.txt", 10000)
-    x_test, y_test = read_data("data/conll2003/test.txt", 1000)
+    x_train, y_train = read_data("data/conll2003/train.txt", 14000)
+    x_test, y_test = read_data("data/conll2003/test.txt", 3200)
     d_x = build_vocab(x_train+x_test)
     d_y = build_vocab(y_train+y_test)
     word2idx = {d_x[i]: i for i in range(len(d_x))}
     tag2idx = {d_y[i]: i for i in range(len(d_y))}
-    tag2idx["<START>"] = 3
-    tag2idx["<STOP>"] = 4
+    tag2idx["<START>"] = 9
+    tag2idx["<STOP>"] = 10
     pad_idx = len(word2idx)
     word2idx['<pad>'] = pad_idx
     tag2idx['<pad>'] = len(tag2idx)
     vocab_size = len(word2idx)
-
+    idx2tag = {value: key for key, value in tag2idx.items()}
+    print(tag2idx)
     return word2idx, tag2idx, vocab_size
 
-word2idx, tag2idx, vocab_size = pre_processing()
+def compute_f1(pred, targets, length_list):
+    tp, fn, fp = [], [], []
+    for i in range(15):
+        tp.append(0)
+        fn.append(0)
+        fp.append(0)
+    for i, length in enumerate(length_list):
+        for j in range(length):
+            a, b = pred[i][j], targets[i][j]
+            if (a == b):
+                tp[a] += 1
+            else:
+                fp[a] += 1
+                fn[b] += 1
+    tps = 0
+    fps = 0
+    fns = 0
+    for i in range(9):
+        tps += tp[i]
+        fps += fp[i]
+        fns += fn[i]
+    p = tps / (tps + fps)
+    r = tps / (tps + fns)
+    return 2 * p * r / (p + r)
+
+
